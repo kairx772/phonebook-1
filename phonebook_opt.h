76560c72a8b4e8b20a6d51b375f2ed7a6c060de1
kairx772 <kaoweitse@lolab-nthu.org>
2017-02-28 14:38:15 +0800

    Change the structure of phonebook_opt.h

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,10 +5,9 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
-    char firstName[16];
+#define OPT 1
+
+typedef struct __PHONE_BOOK_DETAIL {
     char email[16];
     char phone[10];
     char cell[10];
@@ -17,6 +16,11 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} detail;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    detail *pInfo;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
